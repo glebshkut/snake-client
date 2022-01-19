a2d6618d3dbe65233135e04da0093c2305963e88
Gleb Shkut <gleb.shkut@gmail.com>
2022-01-19 13:34:37 -0800

    snake appears with the name

---
+++ play.js
@@ -0,0 +1,27 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: '165.227.47.243',
+    port: 50541
+  });
+
+  conn.on('data', (data) => {
+    console.log("data: " + data.toString());
+  })
+
+  conn.on('connect', () => {
+    console.log("Successfully connected to game server");
+  })
+
+  conn.write('Name: GLB');
+
+  conn.setEncoding("utf8");
+
+  return conn;
+};
+
+console.log("Connecting ...");
+
+module.exports = connect;
